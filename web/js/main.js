485fe378ff87e434b9df43a0f10955352c5c7cf4
AlTosterino <altosterino@gmail.com>
2020-02-23 19:21:00 +0100

    Add adding and receiving messages, auto scroll and auto focus.

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,27 @@
-var socket = io("http://localhost:5000");
+var socket = io("http://192.168.0.169:5000");
 var username = null;
 
 var username_container = document.getElementById("username_container");
 var username_input = document.getElementById("username");
 var username_submit = document.getElementById("username_submit");
+
 var chat_container = document.getElementById("chat_container");
 
-socket.on('connect', _ => {
+var chat_message_input = document.getElementById("chat_message_input");
+var chat_message_submit = document.getElementById("chat_message_submit");
+var chat_message_mock = document.getElementById("chat_message_mock");
+var chat_messages_container = document.getElementById("chat_messages_container");
+
+socket.on("connect", _ => {
     console.log("Connected");
+})
+
+socket.on("message", (data) => {
+    // Show message only if user has picked username
+    if(username){
+        show_message(data.message, data.username, false);
+        chat_messages_container.scrollTop = chat_messages_container.scrollHeight;
+    }
 })
 
 
@@ -30,7 +44,7 @@ function username_empty(){
 function hide_username_picker(){
     fade_out(username_container, 60).then(() => {
         fade_in(chat_container, 80).then(() => {
-            return;
+            chat_message_input.focus();
         })
     });
 }
@@ -66,10 +80,6 @@ function fade_in(element, timeout){
     })
 }
 
-function show_chat(){
-    
-}
-
 function check_username(_username){
     if(!_username){
         username_empty();
@@ -80,9 +90,35 @@ function check_username(_username){
             }else{
                 username = _username;
                 hide_username_picker();
-                show_chat();
             }
         });  
+    }
+}
+
+function show_message(_message, _username, user){
+    let message_mock = chat_message_mock.cloneNode(true);
+    let chat_username = message_mock.querySelector(".chat_username");
+    let chat_message = message_mock.querySelector(".chat_user_message");
+    chat_username.innerHTML = _username;
+    chat_message.innerHTML = _message;
+    message_mock.style.display = "";
+    if (user){
+        message_mock.classList.add("justify-content-end");
+    }
+    chat_messages_container.appendChild(message_mock);
+    chat_messages_container.scrollTop = chat_messages_container.scrollHeight;
+    chat_message_input.focus();
+}
+
+function broadcast_message(_message){
+    socket.emit('message', _message);
+}
+
+function check_message(_message){
+    if(_message){
+        broadcast_message(_message, username);
+        show_message(_message, username, true);
+        chat_message_input.value = "";
     }
 }
 
@@ -95,4 +131,14 @@ username_input.addEventListener("keyup", function(event){
 
 username_submit.addEventListener("click", _ => {
     check_username(username_input.value);
+});
+
+chat_message_input.addEventListener("keyup", _ => {
+    if (event.keyCode == 13){
+        check_message(chat_message_input.value);
+    }
+});
+
+chat_message_submit.addEventListener("click", _ => {
+        check_message(chat_message_input.value);
 });
